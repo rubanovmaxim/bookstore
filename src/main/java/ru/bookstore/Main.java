8c26d15a5731a27e32767ed4f87542d862f2e76f
rubanovmaxim <maksru@mail.ru>
2019-12-01 23:28:12 +0300

    тестовый сервис

---
+++ src/main/java/ru/bookstore/Main.java
@@ -0,0 +1,24 @@
+package ru.bookstore;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@SpringBootApplication
+public class Main {
+
+
+    @GetMapping("/")
+    public String home() {
+        return "TestASas";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}

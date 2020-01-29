6b266bff4a535b322ebd99e7df46c9401f24b1ca
Rubanov.Maksim <Maksim.Rubanov@infotecs.ru>
2020-01-29 19:53:34 +0300

    Настроил Nats

--- src/main/java/ru/bookstore/rest/NotificationController.java
+++ src/main/java/ru/bookstore/rest/NotificationController.java
@@ -6,11 +6,9 @@ import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +18,6 @@ import ru.bookstore.domain.User;
 import ru.bookstore.domain.enums.NotificationType;
 import ru.bookstore.events.NotificationEvent;
 import ru.bookstore.repositories.UserRepository;
-import ru.bookstore.services.MyBean;
 
 import java.util.Optional;
 
@@ -34,19 +31,6 @@ public class NotificationController {
     private ApplicationEventPublisher applicationEventPublisher;
 
     private UserRepository userRepository;
-//
-//    @Autowired
-//    private MyBean myBean;
-//
-@Value(value = "${kafka.topic-name}")
-private String topicName;
-
-    @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
-
-    public void sendMessage(String msg) {
-        kafkaTemplate.send(topicName, msg);
-    }
 
     @Autowired
     public NotificationController(ApplicationEventPublisher applicationEventPublisher, UserRepository userRepository) {
@@ -57,10 +41,6 @@ private String topicName;
     @ApiOperation(value = "Метод отправляет сообщение пользователю на почту и телефон.", response = String.class, tags = "sendNotification")
     @PostMapping("/notification/message")
     public ResponseEntity sendNotification(@RequestBody(required = true) Notification notification) {
-        sendMessage("aqsdasd");
-
-        if(true) return ResponseEntity.ok().build();
-
         Optional<User> userOpt = userRepository.findById(notification.getUserId());
         if (!userOpt.isPresent()) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("There are no user with id=" + notification.getUserId());
